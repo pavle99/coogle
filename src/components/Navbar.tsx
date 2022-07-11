a67559be7ab2e55e4c9acb26399da5394bb15701
pavle99 <mi18024@alas.matf.bg.ac.rs>
2022-07-11 12:09:12 +0200

    add SiteRoutes component and enable site navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,31 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import Search from "./Search"
+
 interface NavProps {
-  darkTheme: boolean;
-  setDarkTheme: React.Dispatch<React.SetStateAction<boolean>>;
+  darkTheme: boolean,
+  setDarkTheme: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-const Navbar: React.FC<NavProps> = ({ darkTheme, setDarkTheme }) => {
+const Navbar: React.FC<NavProps> = ({darkTheme, setDarkTheme}) => {
   return (
-    <div
-      className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center
-          item-center border-b dark:border-gray-700 border-gray-200"
-    >
+    <div className="p-5 pb-0 flex flex-wrap sm:justify-between justify-center
+          item-center border-b dark:border-gray-700 border-gray-200">
       <div className="flex justify-between items-center space-x-5 w-screen">
         <Link to="/">
-          <p className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-500 dark:text-gray-900">
+          <p
+            className="text-2xl bg-blue-500 font-bold text-white py-1 px-2 rounded dark:bg-gray-500 dark:text-gray-900">
             Coogle 🔎
           </p>
         </Link>
-        <button
-          type="button"
-          onClick={() => setDarkTheme(!darkTheme)}
-          className="text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-2 py-1 hover:shadow-lg"
-        >
+        <button type="button" onClick={() => setDarkTheme(!darkTheme)} className="text-xl dark:bg-gray-50
+                                                                                  dark:text-gray-900 bg-white border
+                                                                                  rounded-full px-2 py-1 hover:shadow-lg">
           {darkTheme ? "💡 Light" : "🌙 Dark"}
         </button>
       </div>
+      <Search/>
     </div>
   );
 };
--- src/components/SiteRoutes.tsx
+++ src/components/SiteRoutes.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
+
+import Results from "./Results"
 
 const SiteRoutes = () => {
   return (
-    <div>
-      <h1>SiteRoutes</h1>
+    <div className="p-4">
+      <Routes>
+        <Route path="/" element={<Navigate to="/search" />} />
+        <Route path="/search" element={<Results />}/>
+        <Route path="/images" element={<Results />}/>
+        <Route path="/news" element={<Results />}/>
+        <Route path="/videos" element={<Results />}/>
+      </Routes>
     </div>
   );
 };
